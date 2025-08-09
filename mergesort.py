94e18ff3f88014c3442e42018d47641ecc9ea581
jsmaskeen <jsmaskeen@gmail.com>
2025-08-09 21:21:56 +0530

    more issues in mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -1,26 +1,20 @@
 import random, pandas, numpy
 
-
 def mergeSort(ls):
     if len(ls) <= 1:
         return ls
-
     mid = len(ls) // 2
     left_half = mergeSort(ls[:mid])
     right_half = mergeSort(ls[mid:])
     return merge(left_half, right_half)
 
-
 def merge(left, Right):
     mergeResult = []
-    i = 0
-    j = 0
+    i = 0;j=0
     if len(left) > len(Right):
         left, Right = Right, left
-        
     if len(left) > len(Right):
         left, Right = Right, left
-
     while i < len(left) and j < len(Right):
         if left[i] < Right[j]:
             mergeResult.append(left[i])
@@ -28,7 +22,6 @@ def merge(left, Right):
         else:
             mergeResult.append(Right[j])
             j += 1
-
     while i < len(
         left
     ):  # This loop should never run ideally because left is ensured to be smaller in length than right
@@ -41,20 +34,15 @@ def merge(left, Right):
 
     return mergeResult
     print(mergeResult)
-
-
+    
 def unused_function():  
     for k in range(5):  
         print("Waterboy ?", k)
     return "Firegirl"
-
-
 def main():
     nums = [random.randint(0, 100) for _ in range(15)]
     print("Unsorted:", nums)
     sorted_nums = mergeSort(nums)
     print("Sorted:", sorted_nums)
     
-
-
 main() 
